a78833e31aa1a7ae70c0768dac0ebd1158654595
nathan windmüller <nathan.windmuller@gmail.com>
2019-01-01 23:20:04 +0100

    Update Program.cs

--- Week 4 Periode 2/Opdracht 1/Program.cs
+++ Week 4 Periode 2/Opdracht 1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,45 @@ namespace Opdracht_1
     {
         static void Main(string[] args)
         {
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+        void Start()
+        {
+            string bestand = "test.txt";
+            SchrijfPersoon(LeesPersoon(), bestand);
+            //ToonPersoon();
+            Console.ReadKey();
+        }
+        Persoon LeesPersoon()
+        {
+            Persoon p = new Persoon();
+            Console.Write("Voer je naam in: ");
+            p.Naam = Console.ReadLine();
+            Console.Write("Voer je woonplaats in: ");
+            p.Woonplaats = Console.ReadLine();
+            Console.Write("Voer je leeftijd in: ");
+            p.Leeftijd = int.Parse(Console.ReadLine());
+            return p;
+        }
+        void ToonPersoon(Persoon p)
+        {
+            Console.WriteLine("Naam: " + p.Naam);
+            Console.WriteLine("Woonplaats: " + p.Woonplaats);
+            Console.WriteLine("Leeftijd: " + p.Leeftijd);
+        }
+        void SchrijfPersoon(Persoon p, string bestandsNaam)
+        {
+            StreamWriter writer = new StreamWriter(bestandsNaam);
+            writer.WriteLine(p.Naam);
+            writer.WriteLine(p.Woonplaats);
+            writer.WriteLine(p.Leeftijd);
+            writer.Close();
+        }
+        Persoon LeesPersoon(string bestandsNaam)
+        {
+            StreamReader reader = new StreamReader(bestandsNaam);
+            Console.WriteLine
         }
     }
 }
