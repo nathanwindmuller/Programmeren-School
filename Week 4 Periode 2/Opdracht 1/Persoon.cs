d8573a60b772a9b064e7d9fcfdb113d9f4cd3b5a
nathan windmüller <nathan.windmuller@gmail.com>
2019-01-01 13:57:12 +0100

    opdracht A

---
+++ Week 4 Periode 2/Opdracht 1/Persoon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht_1
+{
+    class Persoon
+    {
+        public string Naam;
+        public string Woonplaats;
+        public int Leeftijd;
+    }
+}
